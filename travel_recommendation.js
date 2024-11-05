8d6ce42dbb0f8cd810b77c78f6997945f8ee6a58
YakeDev <erickay.dev@gmail.com>
2024-11-06 01:16:15 +0200

    Add style on home page and about page with tailwind

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -20,9 +20,11 @@ if (btnClear) {
 	});
 }
 
-btnSearch.addEventListener('click', async () => {
-	await recommendationSearch();
-});
+if (btnSearch) {
+	btnSearch.addEventListener('click', async () => {
+		await recommendationSearch();
+	});
+}
 
 //recommandation search function
 async function recommendationSearch() {
@@ -85,15 +87,27 @@ async function recommendationSearch() {
 			searchResult.innerHTML = filteredResults
 				.map(
 					(item) =>
-						`<div class="result-item">
-							<img src="${item.imageUrl}" alt="${item.name}" />
-							<h3>${item.name}</h3>
-							<p>${item.description}</p>
-						</div>`
+						`
+            <div class="max-w-lg bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-gray-800 dark:border-gray-700 my-5">
+						<a class="card-header">
+							<img src="${item.imageUrl}" alt="${item.name}" class="rounded-t-lg" />
+						</a>
+						<div class="card-description p-6">
+							<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${item.name}</h5>
+							<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${item.description}</p>
+							<a class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Visit 
+							<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+							</a>
+						</div>
+					</div>
+            
+            `
 				)
 				.join('');
 		} else {
-			searchResult.innerHTML = `<p>Aucune recommandation trouvée pour <strong>"${keyWord}"</strong>.</p>`;
+			searchResult.innerHTML = `<p class="my-36 text-gray-700 p-8 bg-gray-100 rounded-2xl">Aucune recommandation trouvée pour <strong>"${keyWord}"</strong>.</p>`;
 		}
 	} else {
 		console.error("Le format des recommandations n'est pas valide.");
